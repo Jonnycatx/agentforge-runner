938a3c9ab44a005a6a88577794c9a2379c6a47d6
jon F <jonfiorucci@gmail.com>
2026-01-29 07:34:58 -0500

    Bump runner version to 2.1.16

--- client/src/components/deployment-modal.tsx
+++ client/src/components/deployment-modal.tsx
@@ -191,7 +191,7 @@ export function DeploymentModal({ open, onOpenChange }: DeploymentModalProps) {
   };
 
   const GITHUB_REPO = "Jonnycatx/agentforge-runner";
-  const RUNNER_VERSION = "2.1.15";
+  const RUNNER_VERSION = "2.1.16";
   const getRunnerDownloadUrl = () => {
     const baseUrl = `https://github.com/${GITHUB_REPO}/releases/latest/download`;
     switch (detectedOS) {
